cbabd686d3116b28992348d039f824106291a204
hashir-cell <hashir@gns-cpas.com>
2026-04-23 00:15:27 +0500

    ..

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://rena-coincident-inviolately.ngrok-free.dev/api/v1',
-  // baseURL: 'http://127.0.0.1:8000/api/v1',
-   headers: {
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
+  headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
-
   },
   credentials: "include",
 });
